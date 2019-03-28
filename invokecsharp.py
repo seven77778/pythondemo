16cc6140db0dfd3e1dbf3de757baa22cd2eeb743
lsh <lsh>
2019-03-28 20:03:30 +0800

    first

---
+++ invokecsharp.py
@@ -0,0 +1,14 @@
+print("this my first python demo on github")
+
+import clr
+
+# the sample method invoke C# dll
+
+from HotelLockDll import *
+clr.FindAssembly("HotelLockDll.dll")
+instance = HotelLockDll()
+print("start")
+print(instance.Initialize())
+print(instance.Read_Card("316091"))
+print(instance.Write_Card("316091", "Li", "10301", "0100", "2019-03-26 22:34:00", "2019-03-27 15:34:00"))
+print("end")
